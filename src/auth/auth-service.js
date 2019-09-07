00f259eb2e047983045a67bc1616d2bab6b4d3e7
Nathan Szelag <nszelag@live.com>
2019-09-07 18:06:57 +0000

    added jwt authentication to routes

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -2,23 +2,28 @@ const jwt = require('jsonwebtoken')
 const config = require('../config')
 
 const AuthService = {
-    getUserWithUserName(db, user_name) {
-      return db('thingful_users')
-        .where({ user_name })
-        .first()
-    },
-    parseBasicToken(token) {
-      return Buffer
-        .from(token, 'base64')
-        .toString()
-        .split(':')
-    },
-    createJWT(subject, payload) {
-      return jwt.sign(payload, config.JWT_SECRET, {
-        subject,
-        algorithm : 'HS256'
-      })
-    }
+  getUserWithUserName(db, user_name) {
+    return db('thingful_users')
+      .where({ user_name })
+      .first()
+  },
+  parseBasicToken(token) {
+    return Buffer
+      .from(token, 'base64')
+      .toString()
+      .split(':')
+  },
+  createJWT(subject, payload) {
+    return jwt.sign(payload, config.JWT_SECRET, {
+      subject,
+      algorithm: 'HS256'
+    })
+  },
+  verifyJWT(token) {
+    return jwt.verify(token, config.JWT_SECRET, {
+      algorithms: ['HS256']
+    })
   }
-  
-  module.exports = AuthService
+}
+
+module.exports = AuthService
--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -9,5 +9,33 @@ const requireAuth = (req, res, next) => {
     } else {
         bearerToken = authToken.slice('bearer '.length, authToken.length)
     }
+    try {
+        const payload = AuthService.verifyJWT(bearerToken)
 
+        AuthService.getUserWithUserName(
+            req.app.get('db'), payload.subject
+        )
+            .then(user => {
+                if (!user) {
+                    return res.status(401).json({
+                        error: 'Unauthorized request'
+                    })
+                }
+
+                req.user = user
+                next()
+            })
+            .catch(err => {
+                console.error(err)
+                next(err)
+            })
+    } catch (error) {
+        res.status(401).json({
+            error: 'Unauthorized request'
+        })
+    }
+}
+
+module.exports = {
+    requireAuth,
 }
