acece9e35e77041a42e4a63c76a703302e094ce6
Nathan Szelag <nszelag@live.com>
2019-09-11 02:01:32 +0000

    added username validation

---
+++ src/users/users-service.js
@@ -0,0 +1,26 @@
+const REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
+
+
+const UsersService = {
+    validatePassword(password) {
+        if (password.length < 8) {
+            return 'Alas, Password be shorter than 8 characters matey. RRRRR!'
+        }
+        if (password.length > 72) {
+            return 'Blimey, password be longer then 72 characters. You scurvy dog!'
+        }
+        if (password.startsWith(' ') || password.endsWith(' ')) {
+            return 'Our ship wasn\'t made for space'
+        }
+        if (!REGEX.test(password)) {
+            return 'Password must contain 1 upper case, lower case, number and special character'
+        }
+        return null
+    },
+    hasUserWithUserName(db, user_name) {
+        return db('thingful_users').where({ user_name }).first()
+            .then(user => !!user)
+    }
+}
+
+module.exports = UsersService
